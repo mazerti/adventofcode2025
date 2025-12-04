d3f0025528a77df1f3519cf172e1d0f4ee0fb7eb
mazerti <mazier@kth.se>
2025-12-04 17:50:55 +0100

    day 2 part 2

--- 2/Repeated.java
+++ 2/Repeated.java
@@ -2,7 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Repeated {
 
@@ -10,6 +13,8 @@ public class Repeated {
         System.out.print("Part One: ");
         partOne(args[0]);
 
+        System.out.print("Part Two: ");
+        partTwo(args[0]);
     }
 
     private static void partOne(String inputFileName) {
@@ -19,9 +24,9 @@ public class Repeated {
             for (String[] range : ranges) {
                 result = result.add(retrieveRepeats(range));
             }
-            System.err.println(result);
+            System.out.println(result);
         } catch (FileNotFoundException e) {
-            System.out.println("Please provide a valid input file.");
+            System.err.println("Please provide a valid input file.");
         }
     }
 
@@ -64,5 +69,94 @@ public class Repeated {
         } catch (Exception e) {
             throw e;
         }
+    }
+
+    private static void partTwo(String inputFileName) {
+        try (Scanner inputReader = new Scanner(new File(inputFileName))) {
+            String inputLine = inputReader.nextLine();
+            BigInteger result = Arrays.stream(inputLine.split("[,]"))
+                    .map(range -> range.split("[-]"))
+                    .map(range -> sumRepeatsInRange(range[0], range[1]))
+                    .reduce(BigInteger.ZERO, BigInteger::add);
+
+            System.out.println(result);
+        } catch (FileNotFoundException e) {
+            System.err.println("Please provide a valid input file.");
+        }
+    }
+
+    private static BigInteger sumRepeatsInRange(String rangeStarts, String rangeEnds) {
+        int minNbDigits = rangeStarts.length();
+        int maxNbDigits = rangeEnds.length();
+
+        Function<Integer, String[]> trimRangeToNbDigits = nbDigits -> {
+            return new String[] { minNbDigits < nbDigits ? "1" + "0".repeat(nbDigits - 1) : rangeStarts,
+                    maxNbDigits > nbDigits ? "9".repeat(nbDigits) : rangeEnds };
+        };
+
+        return IntStream.rangeClosed(minNbDigits, maxNbDigits)
+                .mapToObj(Integer::valueOf)
+                .map(trimRangeToNbDigits)
+                .map(range -> sumRepeatsInRangeFixedDigits(range[0], range[1]))
+                .reduce(BigInteger.ZERO, BigInteger::add);
+    }
+
+    private static BigInteger sumRepeatsInRangeFixedDigits(String rangeStarts, String rangeEnds) {
+        int nbDigits = rangeStarts.length();
+        HashSet<String> matchStore = new HashSet<>();
+        for (int i = 2; i <= nbDigits; i++) {
+            matchStore = listsRepeatsKInRange(i, rangeStarts, rangeEnds, matchStore);
+        }
+        return matchStore.stream()
+                .map(repeats -> new BigInteger(repeats))
+                .reduce(BigInteger.ZERO, BigInteger::add);
+    }
+
+    private static HashSet<String> listsRepeatsKInRange(int k, String rangeStarts, String rangeEnds,
+            HashSet<String> matchStore) {
+        if (rangeStarts.length() % k != 0)
+            return matchStore;
+
+        int kernelSize = rangeStarts.length() / k;
+
+        String minKernel = rangeStarts.substring(0, kernelSize);
+        String maxKernel = rangeEnds.substring(0, kernelSize);
+
+        if (compareStrings(rangeStarts, minKernel.repeat(k)) <= 0
+                && compareStrings(minKernel.repeat(k), rangeEnds) <= 0) {
+            matchStore.add(minKernel.repeat(k));
+        }
+
+        for (int kernel = Integer.parseInt(minKernel) + 1; kernel < Integer.parseInt(maxKernel); kernel++) {
+            matchStore.add(String.valueOf(kernel).repeat(k));
+        }
+
+        if (!(minKernel.equals(maxKernel)) && compareStrings(maxKernel.repeat(k), rangeEnds) <= 0) {
+            matchStore.add(maxKernel.repeat(k));
+        }
+
+        return matchStore;
+    }
+
+    private static int compareStrings(String leftSide, String rightSide) {
+        /*
+         * Returns a positive integer if the left side is a bigger integer than the
+         * right side, a negative integer if the right side is bigger and 0 if both
+         * input are equals.
+         * Expects all input to be positive integer values.
+         * The returned value isn't representative of the difference between the input
+         * values. Only the sign is meaningful.
+         */
+        if (leftSide.equals(rightSide))
+            return 0;
+        if (leftSide.length() - rightSide.length() != 0)
+            return leftSide.length() - rightSide.length();
+        if (leftSide.length() == 1)
+            return Integer.parseInt(leftSide) - Integer.parseInt(rightSide);
+        int headBitDifference = Integer.parseInt(leftSide.substring(0, 1))
+                - Integer.parseInt(rightSide.substring(0, 1));
+        if (headBitDifference != 0)
+            return headBitDifference;
+        return compareStrings(leftSide.substring(1), rightSide.substring(1));
     }
 }
